e88601838ffa3256b579edb74e937b340d054fd9
Marko Paroski <marko.paroski@bloomon.nl>
2018-01-05 10:34:54 +0100

    added style, fixed select all bug

--- jira/js/popup.js
+++ jira/js/popup.js
@@ -1,21 +1,22 @@
 function doCopy(data) {
   var obj =  document.getElementById('copy-clip-div');
 
-  obj.style.display = '';
+  // obj.style.display = '';
 
   obj.contentEditable = true;
   obj.innerHTML = data;
-  obj.unselectable = false;
+  // obj.unselectable = false;
   obj.focus();
   document.execCommand('SelectAll');
   document.execCommand('Copy', false, null);
 
-  obj.style.display = 'none'
+  // obj.style.display = 'none'
 }
 
 function addRuleButton(name, data) {
   var rules = document.getElementById('rules');
   var ruleNode = document.createElement("BUTTON");
+  ruleNode.className = 'max-button';
   rules.appendChild(ruleNode);
 
   ruleNode.appendChild(document.createTextNode("test"));
