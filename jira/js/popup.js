08cdab9d9d56f78aff1ee1a9565de74328ccdd5d
Max Shytikov <mshytikov@gmail.com>
2018-01-10 13:58:37 +0100

    UI adjustments

--- jira/js/popup.js
+++ jira/js/popup.js
@@ -1,5 +1,6 @@
-function doCopy(data) {
+function doCopy(data, close=true) {
   var obj =  document.getElementById('copy-clip-div');
+  obj.style.display = 'block';
 
   obj.contentEditable = true;
   obj.innerHTML = data;
@@ -8,11 +9,14 @@ function doCopy(data) {
   document.execCommand('Copy', false, null);
 
   obj.style.display = 'none';
+  document.getElementById("extension-wrapper").focus();
+  if (close) { window.close() }
 }
 
 function addRuleButton(name, value) {
   var rules = document.getElementById('rules');
   var ruleNode = document.createElement("BUTTON");
+  ruleNode.type="submit";
   ruleNode.className = 'rule-button';
   rules.appendChild(ruleNode);
 
@@ -24,17 +28,12 @@ function addRules(data){
   data.forEach(function(rule) { addRuleButton(rule.name, rule.content) });
 }
 
-function allRulesContent(rules_data){
-  var content = rules_data.map(function(rule) { return rule.content })
-  return content.join('<br>');
-}
-
 
 var bg = chrome.extension.getBackgroundPage();
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   var tab = tabs[0];
   var rules_data = bg.rulesData(tab);
-  doCopy(allRulesContent(rules_data));
+  doCopy(rules_data[0].content, false);
   addRules(rules_data);
 })
